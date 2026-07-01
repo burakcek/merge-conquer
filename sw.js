d857f462efe3e28a40da41e0f8704fa133fab867
burakcek <80116147+burakcek@users.noreply.github.com>
2026-07-01 20:03:38 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'merge-conquer-v1';
+const urlsToCache = [
+  '/merge-conquer/',
+  '/merge-conquer/index.html',
+  '/merge-conquer/icon-192.png',
+  '/merge-conquer/icon-512.png'
+  // Ако имаш звуци, додај ги овде:
+  // '/merge-conquer/sounds/merge.mp3',
+  // '/merge-conquer/sounds/buy.mp3',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
